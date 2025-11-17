5bb5b9aa44cf43341faeab3341de0b6e007fa211
Carson Turner <carsont4@outlook.com>
2025-11-16 23:23:35 -0500

    Theme tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
@@ -10,11 +10,7 @@ import { ThemeService } from './services/theme/theme.service';
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'portfolio-website';
   private themeService = inject(ThemeService);
-
-  ngOnInit(): void {
-    this.themeService.init();
-  }
 }
